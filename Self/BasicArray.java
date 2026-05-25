8e89fe91aa7c270196723e14a086efca973d3ca6
Samia Naz <nsamia312@gmail.com>
2026-05-25 23:56:28 +0500

    Added basic array program (basicArray class) with traversal using for loop

---
+++ Self/BasicArray.java
@@ -0,0 +1,13 @@
+public  class BasicArray {
+
+    public static void main(String[] args) {
+
+        int[] arr = {5, 10, 15, 20, 25};
+
+        System.out.println("Array Elements:");
+
+        for(int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+} 
